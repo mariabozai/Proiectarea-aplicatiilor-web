652892eb878d7da71c5acf34655c3adb10de339b
mariabozai <115458044+mariabozai@users.noreply.github.com>
2024-02-06 09:21:22 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
+import {useHistory, Route} from 'react-router-dom';
 
 import './App.css';
 //import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar"
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { oktaConfig } from './lib/oktaConfig';
+import {OktaAuth, toRelativeUrl} from '@okta/okta-auth-js';
+import { LoginCallback, Security } from '@okta/okta-react';
+import LoginWidget from './Auth/LoginWidget';
+
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const  App = () => {
+
+  const customAuthHandler = () => {
+    history.push('/login');
+  }
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
+
   return (
     <div>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
     <Navbar />
-    <HomePage />
+     <HomePage /> 
+     <SearchBooksPage/> 
+     <Route path='/login' render={
+            () => <LoginWidget config={oktaConfig} /> 
+            } 
+      />
+      <Route path='/login/callback' component={LoginCallback} />
     <Footer />
+    </Security>
     </div>
   );
 }
--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import BookModel from "../../../models/BookModel";
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{book: BookModel}> = (props) => {
     return(
         <div className = "col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3" >
             <div className="text-center">
